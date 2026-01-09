e221c2feac7ff68703f693ae1de4ffafd21e5187
Indus2025 <Indus2025>
2026-01-09 15:52:11 +0400

    fixed hard coded

--- db.php
+++ db.php
@@ -94,5 +94,4 @@ function generateJSONLD($type = 'BlogPosting', $data = []) {
 }
 ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Blog</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
